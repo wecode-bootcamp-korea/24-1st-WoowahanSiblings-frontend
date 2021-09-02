ff652a7b2db6fdad90f8eb6ed6cc1911504c03a8
김세준 <jun@gimsejun-ui-MacBookAir.local>
2021-09-02 09:15:57 +0900

    Modify : 상세페이지 상품설명 탭 sticky, 클릭 이벤트 구현 완료

--- src/pages/Detail/Components/GoodsView.js
+++ src/pages/Detail/Components/GoodsView.js
@@ -2,20 +2,51 @@ import React, { Component } from "react";
 import "./GoodsView.scss";
 
 class GoodsView extends Component {
+  viewTapClick = e => {
+    let targeter = e.target.id;
+    let scrollTarget = document.querySelector("." + targeter);
+    document.querySelector(".viewTapMenu").classList.remove("clicked");
+    e.target.classList.add("clicked");
+    scrollTarget.scrollIntoView();
+  };
+
   render() {
     return (
       <div className="goodsView">
         <div className="goodsViewArea">
           <ul className="goodsVeiwTap">
-            <li className="viewTapMenu">상품설명</li>
-            <li className="viewTapMenu">상세정보</li>
-            <li className="viewTapMenu">후기</li>
+            <li
+              id="detailDesc"
+              className="viewTapMenu"
+              onClick={this.viewTapClick}
+            >
+              상품설명
+            </li>
+            <li
+              id="detailInfo"
+              className="viewTapMenu"
+              onClick={this.viewTapClick}
+            >
+              상세정보
+            </li>
+            <li id="" className="viewTapMenu">
+              후기
+            </li>
             <li className="viewTapMenu">문의</li>
           </ul>
         </div>
 
         <div className="goodsViewContent">
-          <img src="../images/detailpage.png" alt="상세 페이지"></img>
+          <img
+            className="detailDesc"
+            src="../images/detail-desc.png"
+            alt="상세 페이지"
+          ></img>
+          <img
+            className="detailInfo"
+            src="../images/detail-info.png"
+            alt="상세 페이지"
+          ></img>
         </div>
       </div>
     );
